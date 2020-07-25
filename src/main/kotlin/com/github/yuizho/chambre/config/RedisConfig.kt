1359bce74a873a9f9b378c11c68ab8d591107f5b
yuizho <yuizho3@gmail.com>
2020-07-25 15:32:39 +0900

    remove User repository
    because User is member of Room(aagregation class).

--- src/main/kotlin/com/github/yuizho/chambre/config/RedisConfig.kt
+++ src/main/kotlin/com/github/yuizho/chambre/config/RedisConfig.kt
@@ -1,7 +1,6 @@
 package com.github.yuizho.chambre.config
 
 import com.github.yuizho.chambre.domain.room.Room
-import com.github.yuizho.chambre.domain.room.User
 import com.github.yuizho.chambre.presentation.dto.Message
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -18,11 +17,6 @@ import org.springframework.data.redis.stream.StreamReceiver
 class RedisConfig {
     @Bean
     fun messageReactiveRedisOperations(factory: ReactiveRedisConnectionFactory): ReactiveRedisOperations<String, Message> {
-        return createReactiveRedisOperationsByContext(factory)
-    }
-
-    @Bean
-    fun userReactiveRedisOperations(factory: ReactiveRedisConnectionFactory): ReactiveRedisOperations<String, User> {
         return createReactiveRedisOperationsByContext(factory)
     }
 
--- src/main/kotlin/com/github/yuizho/chambre/domain/user/ReactiveUserRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.github.yuizho.chambre.domain.user
-
-import com.github.yuizho.chambre.domain.room.User
-import reactor.core.publisher.Mono
-
-interface ReactiveUserRepository {
-    fun findUserBy(id: String): Mono<User>
-    fun save(user: User): Mono<Boolean>
-}
