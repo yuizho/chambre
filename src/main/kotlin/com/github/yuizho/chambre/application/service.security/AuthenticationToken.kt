fde57b4f959d43fa56fab588b357c7d0f50116c7
yuizho <yuizho3@gmail.com>
2020-07-27 00:10:35 +0900

    rename Approval Authentication modules

--- src/main/kotlin/com/github/yuizho/chambre/application/service.security/AuthenticationToken.kt
+++ src/main/kotlin/com/github/yuizho/chambre/application/service.security/AuthenticationToken.kt
@@ -3,6 +3,18 @@ package com.github.yuizho.chambre.application.service.security
 import com.github.yuizho.chambre.domain.room.User
 import org.springframework.security.authentication.AbstractAuthenticationToken
 
+class ApprovalAuthenticationToken(
+        private val authToken: String
+) : AbstractAuthenticationToken(emptyList()) {
+    override fun getCredentials(): Any {
+        return authToken
+    }
+
+    override fun getPrincipal(): Any? = null
+
+    override fun getName(): String? = null
+}
+
 class UserAuthenticationToken(
         private val user: User
 ) : AbstractAuthenticationToken(listOf(user.role.grantedAuthority)) {
