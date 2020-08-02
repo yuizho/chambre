130acfbcad8c819db91132f6f1be765b84c68c21
yuizho <yuizho3@gmail.com>
2020-08-02 19:15:57 +0900

    modify a demo page to apply an authentication feature

--- src/main/kotlin/com/github/yuizho/chambre/presentation/controller/site/RoomController.kt
+++ src/main/kotlin/com/github/yuizho/chambre/presentation/controller/site/RoomController.kt
@@ -1,5 +1,6 @@
 package com.github.yuizho.chambre.presentation.controller.site
 
+import org.springframework.security.core.context.ReactiveSecurityContextHolder
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -17,6 +18,11 @@ class RoomController {
         //model.addAttribute("items", ReactiveDataDriverContextVariable(flux, 1))
         val uuid = UUID.randomUUID().toString()
         model.addAttribute("uuid", Mono.just(uuid))
+        model.addAttribute(
+                "context",
+                ReactiveSecurityContextHolder.getContext()
+                        .map { it.authentication.principal }
+        )
         return "index.html"
     }
 }
