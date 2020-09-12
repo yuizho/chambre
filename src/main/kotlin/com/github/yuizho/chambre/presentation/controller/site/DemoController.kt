7fd42a448f369e0969e3e55901338ec76622f4f4
yuizho <yuizho3@gmail.com>
2020-09-12 21:01:23 +0900

    refactor demo controller

--- src/main/kotlin/com/github/yuizho/chambre/presentation/controller/site/DemoController.kt
+++ src/main/kotlin/com/github/yuizho/chambre/presentation/controller/site/DemoController.kt
@@ -5,16 +5,12 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import reactor.core.publisher.Mono
-import java.util.*
 
 @RequestMapping("/demo")
 @Controller
 class DemoController {
     @GetMapping("/gm")
     fun gm(model: Model): String {
-        val uuid = UUID.randomUUID().toString()
-        model.addAttribute("uuid", Mono.just(uuid))
         model.addAttribute(
                 "context",
                 ReactiveSecurityContextHolder.getContext()
@@ -25,8 +21,6 @@ class DemoController {
 
     @GetMapping("/user")
     fun user(model: Model): String {
-        val uuid = UUID.randomUUID().toString()
-        model.addAttribute("uuid", Mono.just(uuid))
         model.addAttribute(
                 "context",
                 ReactiveSecurityContextHolder.getContext()
