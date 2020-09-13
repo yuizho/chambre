38e57933cb7d01dbf64333142e3457026bc00ba5
yuizho <yuizho3@gmail.com>
2020-09-13 17:00:15 +0900

    add ttl to saving participant operation

--- src/main/kotlin/com/github/yuizho/chambre/infrastructure/repostiory/auth/ReactiveParticipantRepositoryRedis.kt
+++ src/main/kotlin/com/github/yuizho/chambre/infrastructure/repostiory/auth/ReactiveParticipantRepositoryRedis.kt
@@ -1,14 +1,17 @@
 package com.github.yuizho.chambre.infrastructure.repostiory.auth
 
+import com.github.yuizho.chambre.config.RedisProperties
 import com.github.yuizho.chambre.domain.auth.Participant
 import com.github.yuizho.chambre.domain.auth.ReactiveParticipantRepository
 import org.springframework.data.redis.core.ReactiveRedisOperations
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
+import java.time.Duration
 
 @Repository
 class ReactiveParticipantRepositoryRedis(
-        private val redisOperations: ReactiveRedisOperations<String, Participant>
+        private val redisOperations: ReactiveRedisOperations<String, Participant>,
+        private val redisProperties: RedisProperties
 ) : ReactiveParticipantRepository {
     override fun findParticipantBy(id: Participant.Id): Mono<Participant> {
         return redisOperations
@@ -19,6 +22,10 @@ class ReactiveParticipantRepositoryRedis(
     override fun save(participant: Participant): Mono<Boolean> {
         return redisOperations
                 .opsForValue()
-                .set(participant.token.getIdIdWithSchemaPrefix(), participant)
+                .set(
+                        participant.token.getIdIdWithSchemaPrefix(),
+                        participant,
+                        Duration.ofSeconds(redisProperties.expireSec)
+                )
     }
 }
