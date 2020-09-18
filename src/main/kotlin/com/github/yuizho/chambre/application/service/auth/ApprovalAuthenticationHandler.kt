88a9e670285bee6fd3392c450dcd6e3c49772533
yuizho <yuizho3@gmail.com>
2020-09-18 22:16:04 +0900

    add cookie when authentication is succeeded

--- src/main/kotlin/com/github/yuizho/chambre/application/service/auth/ApprovalAuthenticationHandler.kt
+++ src/main/kotlin/com/github/yuizho/chambre/application/service/auth/ApprovalAuthenticationHandler.kt
@@ -1,6 +1,7 @@
 package com.github.yuizho.chambre.application.service.auth
 
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseCookie
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.server.WebFilterExchange
@@ -9,10 +10,24 @@ import org.springframework.security.web.server.authentication.ServerAuthenticati
 import reactor.core.publisher.Mono
 
 class ApprovalAuthenticationSuccessHandler : ServerAuthenticationSuccessHandler {
+    companion object {
+        // TODO: propertyで名前変えられるようにしたい
+        const val TOKEN_COOKIE_NAME = "chambre-token"
+    }
+
     override fun onAuthenticationSuccess(
             webFilterExchange: WebFilterExchange,
             authentication: Authentication
     ): Mono<Void> = Mono.fromRunnable {
+        // TODO: アプリケーション側に渡すJWTを作成して設定できるようにする
+        // TODO: cookieのpath, secure, httponly, expirationなどを設定できるようにする
+        webFilterExchange.exchange.response.cookies.add(
+                TOKEN_COOKIE_NAME,
+                ResponseCookie.from(
+                        TOKEN_COOKIE_NAME,
+                        authentication.principal.toString().replace(", ", ".")
+                ).build()
+        )
         webFilterExchange.exchange.response.statusCode = HttpStatus.OK
     }
 }
