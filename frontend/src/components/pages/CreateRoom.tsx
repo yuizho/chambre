f612439ee2fde7f2f7c9c9e7880923052c4e38c9
yuizho <yuizho3@gmail.com>
2020-12-19 16:05:38 +0900

    add React Router

---
+++ frontend/src/components/pages/CreateRoom.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+const CreateRoom: FC = () => (
+  <>
+    <h1>create room</h1>
+  </>
+);
+
+export default CreateRoom;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
