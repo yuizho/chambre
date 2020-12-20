d6995aa0423f9fcf6ba5014cef678a54e9622cee
yuizho <yuizho3@gmail.com>
2020-12-20 17:38:09 +0900

    apply atomic design to Room page

--- frontend/src/components/pages/Room.tsx
+++ frontend/src/components/pages/Room.tsx
@@ -1,7 +1,7 @@
-import { Avatar, Text, Flex, Box, Badge } from '@chakra-ui/react';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import useUsers from '../../hooks/use-users';
+import UserList from '../organisms/UserList';
 
 type ParamType = {
   roomId: string;
@@ -14,28 +14,7 @@ const Room = () => {
 
   return (
     <>
-      <Flex>
-        {users.map((user) => (
-          <Box
-            d="flex"
-            p={2}
-            w="6em"
-            h="6em"
-            borderWidth="1px"
-            borderRadius="lg"
-            overflow="hidden"
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Avatar size="sm" />
-            {user.role === 1 ? <Badge colorScheme="green">Host</Badge> : <></>}
-            <Text mt={1} fontWeight="bold">
-              {user.name}
-            </Text>
-          </Box>
-        ))}
-      </Flex>
+      <UserList users={users} />
     </>
   );
 };
