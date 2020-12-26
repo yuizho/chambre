9b9fe660c87211b5b471079ba355a19723c904e5
yuizho <yuizho3@gmail.com>
2020-12-26 22:05:32 +0900

    modify EventList to apply refactoring

---
+++ frontend/src/components/molecules/Eventitem.tsx
@@ -0,0 +1,42 @@
+import { Box, Button, Text } from '@chakra-ui/react';
+import React, { FC, useState } from 'react';
+import useApprove from '../../hooks/use-approve';
+import { PushedMessage } from '../../hooks/use-event';
+
+type Prop = {
+  event: PushedMessage;
+};
+
+const EventItem: FC<Prop> = ({ event }) => {
+  const [useApproveProp, setUseApproveProp] = useState({
+    userId: '',
+    userName: '',
+  });
+
+  useApprove(useApproveProp);
+
+  return (
+    <>
+      <Box key={event.eventId} display="flex" alignItems="center" flexGrow={1}>
+        <Text key={event.userId}>
+          {event.name} が部屋の参加の承認をもとめています
+        </Text>
+        <Button
+          mt={3}
+          size="xs"
+          colorScheme="teal"
+          onClick={() =>
+            setUseApproveProp({
+              userId: event.userId,
+              userName: event.name,
+            })
+          }
+        >
+          approve
+        </Button>
+      </Box>
+    </>
+  );
+};
+
+export default EventItem;
